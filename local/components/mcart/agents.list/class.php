1e1f197587be2923942a4534ee43ad18eb1b635c
Mike Kolotovich <k2punk@gmail.com>
2024-12-11 14:25:04 +0300

    fix class.php in task 177515

--- local/components/mcart/agents.list/class.php
+++ local/components/mcart/agents.list/class.php
@@ -180,6 +180,9 @@ class AgentsList extends CBitrixComponent implements Controllerable, Errorable
          */ 
         //  $this->arResult['STAR_AGENTS'] = CUserOptions::GetOption($category, $name);
          $this->arResult['STAR_AGENTS'] = CUserOptions::GetOption('mcart_agent', 'options_agents_star');
+         if (!is_array($this->arResult['STAR_AGENTS'])) {
+            $this->arResult['STAR_AGENTS'] = [];
+         }
         /*
          * Данного метода нет в документации, код метода и его параметры можно найти в ядре (/bitrix/modules/main/) или в гугле
          * $category - это категория настройки, можете придумать любую, например mcart_agent
